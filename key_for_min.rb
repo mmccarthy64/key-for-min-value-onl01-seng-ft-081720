1c96f9787f5198df56d8d5f9f65ca5f270b89141
Matthew McCarthy <mmccarthy64@hotmail.com>
2020-08-24 23:04:27 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,8 +7,8 @@ def key_for_min_value(name_hash)
   
   name_hash.each do |name, value|
     if lowest_value == 0 || value < lowest_value
-      value = lowest_value
-      name = lowest_name
+      lowest_value = value
+      lowest_name = name
     end
   end
   lowest_name
